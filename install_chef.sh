81c48ad89c4f604f9ef97c915ec4a07926fe8bf8
John Sanabria <john@worker.(none)>
2014-04-29 14:44:31 -0500

    Including support for deploying Chef on CentOS systems

--- install_chef.sh
+++ install_chef.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
-for i in  "$@"; do
-	scp chef__amd64.deb ${i}:.
-	ssh ${i} sudo dpkg -i chef__amd64.deb
-done
+distro=""
+if [ ! $# == 2 ]; then
+	echo "Provides a distro (e.g. ubuntu or centos) and ssh end-point (e.g. someuser@somehost)"
+	exit -1
+else
+	if [[ "${1}" == "ubuntu"  ||  "${1}" == "centos" ]]; then
+		distro=${1}
+	else
+		echo "Distro is not supported ${1}"
+		exit -1
+	fi
+fi
+# Check if chef-solo is installed
+ssh ${2} which chef-solo
+if [ ! $? = 0 ]; then
+	if [ "x$distro" == "xubuntu" ];  then
+		scp chef__amd64.deb ${2}:.
+		ssh ${2} sudo dpkg -i chef__amd64.deb
+	else
+		scp chef-11.4.0-1.el6.x86_64.rpm ${2}:.
+		ssh ${2} sudo rpm -ivh chef-11.4.0-1.el6.x86_64.rpm
+	fi
+fi 
+# Check if rsync is installed
+ssh ${2} which rsync
+if [ ! $? = 0 ]; then
+	if [ "x$distro" == "xubuntu" ];  then
+		ssh ${2} sudo apt-get install rsync
+	else
+		ssh ${2} sudo yum install rsync -y
+	fi
+
+fi
